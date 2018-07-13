8fa9abb4de5d5f4e13401632edf42df249ea8c9f
Root <robhunn74@gmail.com>
2018-07-13 10:34:45 -0500

    Node server is alive!

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require ('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+const app = express();
+const cors=require('cors');
+
+//const session = require('express-session');
+//const enterTheMatrix = require('./redPillorBluePill');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+massive( process.env.SWORDFISH_CONN ).then( dbInstance => {
+    app.set('db', dbInstance)
+} ).catch(err => console.log(err));
+
+//app.post( '/api/addListing', enterTheMatrix.postNewListing );
+//app.get( '/api/fetchListings', enterTheMatrix.getAllListing );
+// app.put( '/api/updateListing/:id', enterTheMatrix.updateListing );
+// app.delete( '/api/deleteListing/:id',enterTheMatrix.handleDelete );
+
+const port = 4000
+app.listen( port, () => { console.log(`Coming to you live from port ${port} , the hottest port in town!`);});
